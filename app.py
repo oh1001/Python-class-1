129019cddacb780c4b8672752f3285a92d407453
Khongoroo <64673299+oh1001@users.noreply.github.com>
2020-05-02 10:55:17 +0800

    First edit

---
+++ app.py
@@ -0,0 +1,49 @@
+x = 12
+y = 21
+print(x+y)
+x = 12
+y = 22
+print(x+y)
+x = 12
+y = 18
+print(x*y)
+x = 15
+y = 22
+print (x-y)
+x = 12
+y = 3 
+print (x // y)
+first_name = "khongoroo"
+last_name = "otgonbayar"
+print (first_name + last_name)
+x = 20
+y = 10
+print (x * y)
+x = 12
+y = 12
+print (x ** y)
+first_name = "khongoroo"
+last_name = "otgonbayar"
+print (first_name * 2)
+x = 12
+y = 12
+print (x // y)
+x = 12
+y = 12
+print (x % y)
+buhelToo = 123
+butarhaiToo = 15.2
+temdegtMor = "hyz"
+print(type(buhelToo)) # buhel toonii turliig harah
+print(type(butarhaiToo)) # butarhai toonii turliig harah
+print (type(temdegtMor)) # temdet moriin turliig harah
+x = 15
+y = 17.5
+age = "32"
+z = int(age) # str -> int
+t = str (x) # int -> str
+print (age + t)
+
+
+
+
